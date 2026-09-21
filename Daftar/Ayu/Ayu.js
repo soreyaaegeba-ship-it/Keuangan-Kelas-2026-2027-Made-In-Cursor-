3b39c05abc238d0b90beb3797b87bf4f3bef16c0
soreyaaegeba-ship-it <soreyaaegeba@gmail.com>
2026-09-22 04:03:10 +0700

    Add files via upload

---
+++ Daftar/Ayu/Ayu.js
@@ -0,0 +1,69 @@
+const ctx = document.getElementById("grafikKeuangan");
+
+const chart = new Chart(ctx, {
+    type: "line",
+    data: {
+        labels: ["M4", "M5", "M6", "M7", "M8"],
+        datasets: [{
+            label: "Kas(Kekurangan)",
+            data: [0, 4000, 10000, 16000, 15000],
+            borderColor: "red",
+            pointBackground: "red"
+        }]
+    },
+
+    options: {
+        plugins: {
+            legend: {
+                onClick: function(e, legendItem, legend) {
+
+                    if (chart.data.datasets[0].label === "Kas(Kekurangan)") {
+                        chart.data.datasets[0].label = "Kas Pramuka(Kekurangan)";
+                        chart.data.datasets[0].data = [0, 0, 0, 0, 5000];
+                    } else {
+                        chart.data.datasets[0].label = "Kas(Kekurangan)";
+                        chart.data.datasets[0].data = [0, 4000, 10000, 16000, 15000];
+                    }
+
+                    chart.update();
+                }
+            }
+        }
+    }
+});
+
+const ctx1 = document.getElementById("grafikKeuangan1");
+
+const chart1 = new Chart(ctx1, {
+    type: "line",
+    data: {
+        labels: ["M4", "M5", "M6", "M7", "M8"],
+        datasets: [{
+            label: "Kas",
+            data: [0, 10000, 0, 0, 5000],
+        }]
+    },
+
+    options: {
+        plugins: {
+            legend: {
+                onClick: function(e, legendItem, legend) {
+
+                    if (chart1.data.datasets[0].label === "Kas") {
+                        chart1.data.datasets[0].label = "Kas Pramuka";
+                        chart1.data.datasets[0].data = [5000, 0, 0, 0, 0];
+                    } else {
+                        chart1.data.datasets[0].label = "Kas";
+                        chart1.data.datasets[0].data = [0, 10000, 0, 0, 5000];
+                    }
+
+                    chart1.update();
+                }
+            }
+        }
+    }
+});
+
+function out1() {
+    window.location.href="../../index.html";
+};
---
+++ Daftar/Shevana/Shevana.js
@@ -0,0 +1,69 @@
+const ctx = document.getElementById("grafikKeuangan");
+
+const chart = new Chart(ctx, {
+    type: "line",
+    data: {
+        labels: ["M4", "M5", "M6", "M7", "M8"],
+        datasets: [{
+            label: "Kas(Kekurangan)",
+            data: [16000, 20000, 26000, 27000, 31000],
+            borderColor: "red",
+            pointBackground: "red"
+        }]
+    },
+
+    options: {
+        plugins: {
+            legend: {
+                onClick: function(e, legendItem, legend) {
+
+                    if (chart.data.datasets[0].label === "Kas(Kekurangan)") {
+                        chart.data.datasets[0].label = "Kas Pramuka(Kekurangan)";
+                        chart.data.datasets[0].data = [0, 0, 0, 0, 5000];
+                    } else {
+                        chart.data.datasets[0].label = "Kas(Kekurangan)";
+                        chart.data.datasets[0].data = [16000, 20000, 26000, 27000, 31000];
+                    }
+
+                    chart.update();
+                }
+            }
+        }
+    }
+});
+
+const ctx1 = document.getElementById("grafikKeuangan1");
+
+const chart1 = new Chart(ctx1, {
+    type: "line",
+    data: {
+        labels: ["M4", "M5", "M6", "M7", "M8"],
+        datasets: [{
+            label: "Kas",
+            data: [2000, 2000, 0, 5000, 0],
+        }]
+    },
+
+    options: {
+        plugins: {
+            legend: {
+                onClick: function(e, legendItem, legend) {
+
+                    if (chart1.data.datasets[0].label === "Kas") {
+                        chart1.data.datasets[0].label = "Kas Pramuka";
+                        chart1.data.datasets[0].data = [5000, 0, 0, 0, 0];
+                    } else {
+                        chart1.data.datasets[0].label = "Kas";
+                        chart1.data.datasets[0].data = [2000, 2000, 0, 5000, 0];
+                    }
+
+                    chart1.update();
+                }
+            }
+        }
+    }
+});
+
+function out1() {
+    window.location.href="../../index.html";
+};
