8239d36b6451bf305168b24931726e42ef406c11
soreyaaegeba-ship-it <soreyaaegeba@gmail.com>
2026-09-25 02:32:57 +0700

    Add files via upload

--- logo1/analytics/analytics.js
+++ logo1/analytics/analytics.js
@@ -9,7 +9,7 @@ const chart = new Chart(ctx, {
     data: {
         labels: ["M1", "M2", "M3", "M4", "M5", "M6", "M7", "M8", "M9", "M10"],
         datasets: [{
-            label: "Kas(Kekurangan)",
+            label: "Kas Pramuka",
             data: [1, 3, 5, 7, 9, 11, 13, 15, 17, 19],
             borderColor: "red",
             pointBackgroundColor: "red"
@@ -21,11 +21,11 @@ const chart = new Chart(ctx, {
             legend: {
                 onClick: function(e, legendItem, legend) {
 
-                    if (chart.data.datasets[0].label === "Kas(Kekurangan)") {
-                        chart.data.datasets[0].label = "Kas Pramuka(Kekurangan)";
-                        chart.data.datasets[0].data = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20 ];
+                    if (chart.data.datasets[0].label === "Kas Pramuka") {
+                        chart.data.datasets[0].label = "Kas Pramuka";
+                        chart.data.datasets[0].data = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
                     } else {
-                        chart.data.datasets[0].label = "Kas(Kekurangan)";
+                        chart.data.datasets[0].label = "Kas Pramuka";
                         chart.data.datasets[0].data = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
                     }
 
@@ -43,7 +43,7 @@ const chart1 = new Chart(ctx1, {
     data: {
         labels: ["M1", "M2", "M3", "M4", "M5", "M6", "M7", "M8", "M9", "M10"],
         datasets: [{
-            label: "Kas(Kekurangan)",
+            label: "Kas Pramuka",
             data: [1, 3, 5, 7, 9, 11, 13, 15, 17, 19],
             borderColor: "red",
             pointBackgroundColor: "red"
@@ -55,11 +55,11 @@ const chart1 = new Chart(ctx1, {
             legend: {
                 onClick: function(e, legendItem, legend) {
 
-                    if (chart1.data.datasets[0].label === "Kas(Kekurangan)") {
-                        chart1.data.datasets[0].label = "Kas Pramuka(Kekurangan)";
-                        chart1.data.datasets[0].data = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20 ];
+                    if (chart1.data.datasets[0].label === "Kas Pramuka") {
+                        chart1.data.datasets[0].label = "Kas Pramuka";
+                        chart1.data.datasets[0].data = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
                     } else {
-                        chart1.data.datasets[0].label = "Kas(Kekurangan)";
+                        chart1.data.datasets[0].label = "Kas Pramuka";
                         chart1.data.datasets[0].data = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
                     }
 
@@ -77,7 +77,7 @@ const chart2 = new Chart(ctx2, {
     data: {
         labels: ["M1", "M2", "M3", "M4", "M5", "M6", "M7", "M8", "M9", "M10"],
         datasets: [{
-            label: "Kas(Kekurangan)",
+            label: "Kas Pramuka",
             data: [1, 3, 5, 7, 9, 11, 13, 15, 17, 19],
             borderColor: "red",
             pointBackgroundColor: "red"
@@ -89,11 +89,11 @@ const chart2 = new Chart(ctx2, {
             legend: {
                 onClick: function(e, legendItem, legend) {
 
-                    if (chart2.data.datasets[0].label === "Kas(Kekurangan)") {
-                        chart2.data.datasets[0].label = "Kas Pramuka(Kekurangan)";
-                        chart2.data.datasets[0].data = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20 ];
+                    if (chart2.data.datasets[0].label === "Kas Pramuka") {
+                        chart2.data.datasets[0].label = "Kas Pramuka";
+                        chart2.data.datasets[0].data = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
                     } else {
-                        chart2.data.datasets[0].label = "Kas(Kekurangan)";
+                        chart2.data.datasets[0].label = "Kas Pramuka";
                         chart2.data.datasets[0].data = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
                     }
 
@@ -111,8 +111,8 @@ const chart3 = new Chart(ctx3, {
     data: {
         labels: ["M1", "M2", "M3", "M4", "M5", "M6", "M7", "M8", "M9", "M10"],
         datasets: [{
-            label: "Kas(Kekurangan)",
-            data: [1, 3, 5, 7, 9, 11, 13, 15, 17, 19],
+            label: "Kas Kelas",
+            data: [134000, 65000, 140000, 171000, 164000, 14000, 83000, 168000, 42000, 0],
         }]
     },
 
@@ -121,12 +121,12 @@ const chart3 = new Chart(ctx3, {
             legend: {
                 onClick: function(e, legendItem, legend) {
 
-                    if (chart3.data.datasets[0].label === "Kas(Kekurangan)") {
-                        chart3.data.datasets[0].label = "Kas Pramuka(Kekurangan)";
-                        chart3.data.datasets[0].data = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20 ];
+                    if (chart3.data.datasets[0].label === "Kas Kelas") {
+                        chart3.data.datasets[0].label = "Kas Kelas";
+                        chart3.data.datasets[0].data = [134000, 65000, 140000, 171000, 164000, 14000, 83000, 168000, 42000, 0];
                     } else {
-                        chart3.data.datasets[0].label = "Kas(Kekurangan)";
-                        chart3.data.datasets[0].data = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+                        chart3.data.datasets[0].label = "Kas Kelas";
+                        chart3.data.datasets[0].data = [134000, 65000, 140000, 171000, 164000, 14000, 83000, 168000, 42000, 0];
                     }
 
                     chart3.update();
@@ -143,8 +143,8 @@ const chart4 = new Chart(ctx4, {
     data: {
         labels: ["M1", "M2", "M3", "M4", "M5", "M6", "M7", "M8", "M9", "M10"],
         datasets: [{
-            label: "Kas(Kekurangan)",
-            data: [1, 3, 5, 7, 9, 11, 13, 15, 17, 19],
+            label: "Kas Kelas",
+            data: [38000, 103000, 65000, 47000, 48000, 92500, 37000, 235000, 0, 32000],
         }]
     },
 
@@ -153,12 +153,12 @@ const chart4 = new Chart(ctx4, {
             legend: {
                 onClick: function(e, legendItem, legend) {
 
-                    if (chart4.data.datasets[0].label === "Kas(Kekurangan)") {
-                        chart4.data.datasets[0].label = "Kas Pramuka(Kekurangan)";
-                        chart4.data.datasets[0].data = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20 ];
+                    if (chart4.data.datasets[0].label === "Kas Kelas)") {
+                        chart4.data.datasets[0].label = "Kas Kelas";
+                        chart4.data.datasets[0].data = [38000, 103000, 65000, 47000, 48000, 92500, 37000, 235000, 0, 32000];
                     } else {
-                        chart4.data.datasets[0].label = "Kas(Kekurangan)";
-                        chart4.data.datasets[0].data = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+                        chart4.data.datasets[0].label = "Kas Kelas";
+                        chart4.data.datasets[0].data = [38000, 103000, 65000, 47000, 48000, 92500, 37000, 235000, 0, 32000];
                     }
 
                     chart4.update();
@@ -175,8 +175,8 @@ const chart5 = new Chart(ctx5, {
     data: {
         labels: ["M1", "M2", "M3", "M4", "M5", "M6", "M7", "M8", "M9", "M10"],
         datasets: [{
-            label: "Kas(Kekurangan)",
-            data: [1, 3, 5, 7, 9, 11, 13, 15, 17, 19],
+            label: "Kas Kelas",
+            data: [70000, 191000, 257000, 266000, 311000, 487000, 585000, 606000, 569000, 569000],
         }]
     },
 
@@ -185,12 +185,12 @@ const chart5 = new Chart(ctx5, {
             legend: {
                 onClick: function(e, legendItem, legend) {
 
-                    if (chart5.data.datasets[0].label === "Kas(Kekurangan)") {
-                        chart5.data.datasets[0].label = "Kas Pramuka(Kekurangan)";
-                        chart5.data.datasets[0].data = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20 ];
+                    if (chart5.data.datasets[0].label === "Kas Kelas") {
+                        chart5.data.datasets[0].label = "Kas Kelas";
+                        chart5.data.datasets[0].data = [70000, 191000, 257000, 266000, 311000, 487000, 585000, 606000, 569000, 569000]; 
                     } else {
-                        chart5.data.datasets[0].label = "Kas(Kekurangan)";
-                        chart5.data.datasets[0].data = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+                        chart5.data.datasets[0].label = "Kas Kelas";
+                        chart5.data.datasets[0].data = [70000, 191000, 257000, 266000, 311000, 487000, 585000, 606000, 569000, 569000];
                     }
 
                     chart5.update();
